b2a00038913f6c825e4f180b6a27ddf6e129542a
mdduran <mdduran@outlook.com>
2015-04-16 03:03:57 -0400

    Foundation controllers created
    Also adjusted some moves

--- src/mdduran/TableauToTableauMove.java
+++ src/mdduran/TableauToTableauMove.java
@@ -1,11 +1,12 @@
 package mdduran;
 
+import ks.common.model.BuildablePile;
 import ks.common.model.Card;
 import ks.common.model.Column;
 
 public class TableauToTableauMove extends TableauMove{
-	Column fromTableau;	
-	public TableauToTableauMove(Column from, Card cardBeingMoved, Column to, int rankOfTableau){
+	BuildablePile fromTableau;	
+	public TableauToTableauMove(BuildablePile from, Card cardBeingMoved, Column to, int rankOfTableau){
 		super(from, cardBeingMoved, to, rankOfTableau);
 		this.fromTableau = from;
 		
